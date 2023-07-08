1e064d8f3010ec2c1a675fce64156a7027198859
Tan Chin Hiong <tanchinhiong@gmail.com>
2023-07-08 10:43:05 +0800

    Adds config.py to consolidate configuration

--- ghost.py
+++ ghost.py
@@ -11,12 +11,10 @@ from waitress import serve
 import webbrowser
 from datetime import datetime
 
-# get configurations
-load_dotenv(find_dotenv())
-specs_file = os.getenv('SPECS') or 'specs.md' # defaults to specs.md
-provider = os.getenv('PROVIDER') or 'openai' # defaults to openai
-output_file = os.getenv('OUTPUT_FILE') or 'output.md' # defaults to output.md
-retries = int(os.getenv('MAX_RETRIES', 3) or 3) # defaults to 3
+from config import specs_file, output_file, retries, provider
+from config import provider_config as cfg
+
+# Initialize
 llm = None
 
 # get path for static files
@@ -33,77 +31,65 @@ def initAgent():
     with open(specs_file, 'r') as file:
         specs = file.read()
     print(specs, "\nlength:", len(specs), "words")
-    print(f"\033[96mUsing {provider} \033[0m")
+    print(f"\033[96mUsing {cfg.provider} \033[0m")
 
     # OpenAI
-    if provider == "openai":
-        api_key  = os.getenv('OPENAI_API_KEY')
-        model_name = os.getenv('OPENAI_MODEL')
-        api_version = os.getenv('OPENAI_API_VERSION')
-        base_url = os.getenv('OPENAI_API_BASE')        
-
-        if model_name.startswith("gpt-4") or model_name.startswith("gpt-3.5"):
+    if cfg.provider == "openai":
+        if cfg.model_name.startswith("gpt-4") or cfg.model_name.startswith("gpt-3.5"):
             llm = ChatOpenAI(
                 temperature=0.7,
-                model_name=model_name,
-                openai_api_key=api_key,
+                model_name=cfg.model_name,
+                openai_api_key=cfg.api_key,
                 max_retries=retries,
             )      
         else:
             llm = OpenAI(
                 temperature=0.7,
-                model_name=model_name,
-                openai_api_key=api_key,
+                model_name=cfg.model_name,
+                openai_api_key=cfg.api_key,
                 max_retries=retries,
             )      
 
 
     # Azure OpenA
-    if provider == "azure":
-        api_key  = os.getenv('AZURE_API_KEY')
-        model_name = os.getenv('AZURE_MODEL')
-        deployment_name = os.getenv('AZURE_DEPLOYMENT_NAME')
-        api_version = os.getenv('AZURE_API_VERSION')
-        base_url = os.getenv('AZURE_API_BASE')
-
-        if model_name.startswith("gpt-4") or model_name.startswith("gpt-3.5"):                       
+    if cfg.provider == "azure":
+        if cfg.model_name.startswith("gpt-4") or cfg.model_name.startswith("gpt-3.5"):                       
             llm = AzureChatOpenAI(
                 temperature=0.7,
-                openai_api_base=base_url,
-                openai_api_version=api_version,
-                model_name=model_name,
-                deployment_name=deployment_name,
-                openai_api_key=api_key,
+                openai_api_base=cfg.base_url,
+                openai_api_version=cfg.api_version,
+                model_name=cfg.model_name,
+                deployment_name=cfg.deployment_name,
+                openai_api_key=cfg.api_key,
                 max_retries=retries,
                 openai_api_type = "azure",
             )   
         else:
             llm = AzureOpenAI(
                 temperature=0.7,
-                openai_api_base=base_url,
-                openai_api_version=api_version,
-                model_name=model_name,
-                deployment_name=deployment_name,
-                openai_api_key=api_key,
+                openai_api_base=cfg.base_url,
+                openai_api_version=cfg.api_version,
+                model_name=cfg.model_name,
+                deployment_name=cfg.deployment_name,
+                openai_api_key=cfg.api_key,
                 max_retries=retries,
                 openai_api_type = "azure",
             )   
 
     # Google Vertex AI (PaLM)
-    if provider == "palm":
-        model_name = os.getenv('PALM_MODEL', 'chat-bison')
-        if model_name == "chat-bison" or model_name == "codechat-bison":
+    if cfg.provider == "palm":
+        if cfg.model_name == "chat-bison" or cfg.odel_name == "codechat-bison":
             llm = ChatVertexAI(
                 temperature=0.7,
-                model_name=model_name,
-                location=os.getenv('PALM_LOCATION', 'us-central1'),
+                model_name=cfg.model_name,
+                location=cfg.location,
                 max_output_tokens=1024,
             )
         else:
             llm = VertexAI(
                 temperature=0.7,
-                model_name=model_name,
-                location=os.getenv('PALM_LOCATION', 'us-central1'),
+                model_name=cfg.model_name,
+                location=cfg.location,
                 max_output_tokens=1024,
             )
 
